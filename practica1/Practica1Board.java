244dd3cc13fc84ddc008c19ee7f3b66a79eaa82e
jrryy <jerry.om.95@gmail.com>
2016-03-08 11:50:24 +0100

    empezando representación

--- practica1/Practica1Board.java
+++ practica1/Practica1Board.java
@@ -1,3 +1,14 @@
 /*!
  * REpresentacion del estado de la practica 1
  */
+package practica1;
+
+public class Practica1Board {
+    private int nServ; //Numero de servidores
+    private int nUser; //Numero de usuarios
+    private int nUsMaxReq; //Numero de requests maximas por usuario
+    private int nReq; //Numero de requests
+    private int nepl; //Numero de replicaciones minimas
+    private int servReq[]; //Un vector de nReq posiciones indicando en cada posición, equivalente al request, a que servidor lo envia
+
+}
