66226b8f30c505d6916ca0d5be1c5d5913a516c5
jrryy <jerry.om.95@gmail.com>
2016-03-09 12:00:49 +0100

    empezando representación

--- practica1/Practica1Board.java
+++ practica1/Practica1Board.java
@@ -1,6 +1,8 @@
 /*!
  * REpresentacion del estado de la practica 1
  */
+import aima.*;
+import IA.DistFS.*;
 package practica1;
 
 public class Practica1Board {
@@ -8,7 +10,24 @@ public class Practica1Board {
     private int nUser; //Numero de usuarios
     private int nUsMaxReq; //Numero de requests maximas por usuario
     private int nReq; //Numero de requests
-    private int nepl; //Numero de replicaciones minimas
+    private int nRepl; //Numero de replicaciones minimas
+    private int seed;
     private int servReq[]; //Un vector de nReq posiciones indicando en cada posición, equivalente al request, a que servidor lo envia
+    private Requests requests; //Requests
+    private Servers servers; //Servidores
+
+    public Practica1Board(int users, int requs, int servs, int repls, int seed){
+        this.nUser = users;
+        this.nServ = servs;
+        this.nReq = requs;
+        this.nRepl = repls;
+        this.seed = seed;
+        this.requests = new Requests(users,requs,seed);
+        this.servers = new Servers(servs,repls,seed);
+    }
+
+     public static void main(String[] args){
+
+     }
 
 }
--- practica1/Practica1GoalTest.java
+++ practica1/Practica1GoalTest.java
@@ -1,1 +1,13 @@
 //como acaba
+package practica1;
+
+import aima.search.framework.GoalTest;
+
+public class CentralsEstatFinal implements GoalTest {
+
+    public boolean isGoalState(Object state) {
+        return ((Practica1Board)(state)).isGoalState();
+
+    }
+
+}
