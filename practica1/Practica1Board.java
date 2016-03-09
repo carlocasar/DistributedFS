bb4baed0442affa0732f00502f287748aa4272fb
mcdinero <camilo.andres.castro@est.fib.upc.edu>
2016-03-09 17:10:01 +0100

    Ahora mismo ya se puede empezar a trabajar en las soluciones

--- practica1/Practica1Board.java
+++ practica1/Practica1Board.java
@@ -1,9 +1,10 @@
 /*!
- * REpresentacion del estado de la practica 1
+ * Representacion del estado de la practica 1
  */
-import aima.*;
-import IA.DistFS.*;
-package practica1;
+import IA.DistFS.Requests;
+import IA.DistFS.Servers;
+
+import java.util.Scanner;
 
 public class Practica1Board {
     private int nServ; //Numero de servidores
@@ -16,18 +17,35 @@ public class Practica1Board {
     private Requests requests; //Requests
     private Servers servers; //Servidores
 
-    public Practica1Board(int users, int requs, int servs, int repls, int seed){
+    public Practica1Board(int users, int requs, int servs, int repls, int seed) {
         this.nUser = users;
         this.nServ = servs;
         this.nReq = requs;
         this.nRepl = repls;
         this.seed = seed;
         this.requests = new Requests(users,requs,seed);
-        this.servers = new Servers(servs,repls,seed);
+        try {
+            this.servers = new Servers(servs,repls,seed);
+        } catch (Servers.WrongParametersException e) {
+            e.printStackTrace();
+        }
     }
 
      public static void main(String[] args){
 
+         Scanner s = new Scanner(System.in);
+        System.out.println("Introduce seed, numero de usuarios, numero de requests, numero de servers, numero de replicaciones:");
+         int seed = s.nextInt();
+         int nUsers = s.nextInt();
+         int nRequs = s.nextInt();
+         int nServ = s.nextInt();
+         int nRepls = s.nextInt();
+         Practica1Board board = new Practica1Board(nUsers,nRequs,nServ,nRepls,seed);
+         System.out.println("Ahora haré print de todos los datos para comprobar que realmente crea lo que queremos:");
+        for (int i = 0; i < nServ; ++i){
+            System.out.print(board.requests.getRequest(i)[0] + " ");
+            System.out.println(board.requests.getRequest(i)[1]);
+        }
      }
 
 }
--- practica1/Practica1GoalTest.java
+++ practica1/Practica1GoalTest.java
@@ -1,13 +1,1 @@
 //como acaba
-package practica1;
-
-import aima.search.framework.GoalTest;
-
-public class CentralsEstatFinal implements GoalTest {
-
-    public boolean isGoalState(Object state) {
-        return ((Practica1Board)(state)).isGoalState();
-
-    }
-
-}
