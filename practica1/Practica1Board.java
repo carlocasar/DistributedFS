3fcd575458e6ef65e36dd980db14494aef586021
mcdinero <camilo.andres.castro@est.fib.upc.edu>
2016-03-11 17:43:25 +0100

    Solución 1 hecha, empezaré a trabajar en el main de verdad

--- practica1/Practica1Board.java
+++ practica1/Practica1Board.java
@@ -3,7 +3,11 @@
  */
 import IA.DistFS.Requests;
 import IA.DistFS.Servers;
+
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Vector;
 
 public class Practica1Board {
     private int nServ; //Numero de servidores
@@ -12,7 +16,7 @@ public class Practica1Board {
     private int nReq; //Numero de requests
     private int nRepl; //Numero de replicaciones minimas
     private int seed;
-    private int servReq[]; //Un vector de nReq posiciones indicando en cada posición, equivalente al request, a que servidor lo envia
+    private Vector<Integer> servReq; //Un vector de nReq posiciones indicando en cada posición, equivalente al request, a que servidor lo envia
     private Requests requests; //Requests
     private Servers servers; //Servidores
 
@@ -41,10 +45,37 @@ public class Practica1Board {
          int nRepls = s.nextInt();
          Practica1Board board = new Practica1Board(nUsers,nRequs,nServ,nRepls,seed);
          System.out.println("Ahora haré print de todos los datos para comprobar que realmente crea lo que queremos:");
-        for (int i = 0; i < nServ; ++i){
+         System.out.println("Número de requests totales: " + board.requests.size());
+        for (int i = 0; i < board.requests.size(); ++i){
             System.out.print(board.requests.getRequest(i)[0] + " ");
             System.out.println(board.requests.getRequest(i)[1]);
         }
+         board.servReq = solIni1(board);
+         System.out.println("Ahora saco por pantalla las requests y a que servidor iran:");
+         for (int i = 0; i < board.requests.size(); ++i){
+             System.out.println("Request " + i + " servidor " + board.servReq.get(i));
+         }
      }
+
+    /*
+    Este algoritmo encuentra una solución muy sencilla: pregunta que servidores contienen el archivo del que se hace
+     request y, al primero que encuentra, le envia la request.
+     */
+    public static Vector<Integer> solIni1(Practica1Board board){
+        Vector<Integer> servReq = new Vector<Integer>(board.requests.size());
+        //vamos a iterar sobre todas las request para asignarles un servidor
+        for (int i = 0; i < board.requests.size(); ++i) {
+            //aqui se coge el archivo
+            int fileReq = board.requests.getRequest(i)[1];
+            //se pregunta en que servidores esta
+            Set<Integer> set = board.servers.fileLocations(fileReq);
+            //aqui se hace el proceso de iterar sobre el set. No se hace mas que una vez porque es un algoritmo de
+            //mierda, pero se tendria que hacer un for para iterar sobre los demas
+            Iterator<Integer> it = set.iterator();
+            Integer server = it.next();
+            servReq.add(i, server);
+        }
+        return servReq;
+    }
 
 }
