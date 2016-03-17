02f1818436634551fcc0c9d63aae86c31a278d4b
victor-gonzalez-prieto <victor.gonzalez.prieto@gmail.com>
2016-03-17 01:43:44 +0100

    Implementado GoalTest, perfilada Demo. Warning: no junteis los dos branches que el mío no funciona. No se porqué no me coje bien las clases del AIMA, hay que integrar esa parte.

--- DistributedFS/Demo.java
+++ DistributedFS/Demo.java
@@ -1,1 +1,36 @@
-//El que inicia
+
+import aima.search.framework.Problem;
+import aima.search.framework.SuccessorFunction;
+
+public class Demo {
+
+    public static void HillClimbing(int users, int reqs, int servs, int repls, int seed, int iSol, int sGen) {
+        System.out.println("describir la demo, sus parametros, o algo");
+
+        Board initSol = new Board(users,reqs,servs,repls,seed);
+        switch (iSol) {}
+
+        SuccessorFunction sucGen;
+        switch (sGen) {
+            case 1 : sucGen = new SuccessorFunctionHC1();
+                break;
+            case 2 : sucGen = new SuccessorFunctionHC2();
+                break;
+            case 3 : sucGen = new SuccessorFunctionHC3();
+                break;
+            default : sucGen = null;
+        }
+
+        try {
+            Problem e = new Problem(initSol, sucGen, new GoalFalseTest());
+
+            /* ¿? */
+
+        } catch (Exception var4) {
+            var4.printStackTrace();
+        }
+    }
+
+    public static void SimmulatedAnnealing(/*parametros*/) {}
+
+}
--- DistributedFS/HeuristicFunction.java
+++ DistributedFS/HeuristicFunction.java
@@ -1,1 +1,5 @@
 //funcion heuristica
+
+
+// La de minimizar el tiempo que tarda el servidor más cargado es easy y sólo haremos una.
+// Para el otro criterio tendremos que hacer experimentos sobre como combinar media y desviación.
